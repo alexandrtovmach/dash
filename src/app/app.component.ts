5dd74aa87c1395d04233a86ac97be90379d43b02
Alexandr Tovmach <alexandrtovmach@gmail.com>
2018-01-09 22:12:09 +0200

    start implementing div's oriented table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { DataSource } from '@angular/cdk/collections';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { startWith } from 'rxjs/operators/startWith';
+import { map } from 'rxjs/operators/map';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss', './new-styles.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  inlineDatePicker;
+  doctorCtrl: FormControl;
+  filteredDoctors: Observable<any[]>;
+  show = false;
+  days = []
+
+  doctors = [
+    {
+      nameEnglish: 'Dr. Ahmed Mohsen'
+    },
+    {
+      nameEnglish: 'Dr. Mohamed Hassan'
+    },
+    {
+      nameEnglish: 'Dr. Mohamed Nasr'
+    }
+  ];
+  quarterHours = ['00', '15', '30', '45'];
+  times = [];
+
+
+  constructor() {
+    this.doctorCtrl = new FormControl();
+    this.filteredDoctors = this.doctorCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(doctor => doctor ? this.filterDoctors(doctor) : this.doctors.slice())
+      );
+
+    for (let i = 8; i < 24; i++) {
+      for (let j = 0; j < 4; j++) {
+        let time = i + ':' + this.quarterHours[j];
+        if (i < 10) {
+          time = '0' + time;
+        }
+        this.times.push(time);
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.show = true;
+    console.log(this.days)
+  }
+
+
+  filterDoctors(name) {
+    return this.doctors.filter(doctor => doctor.nameEnglish.toLowerCase().indexOf(name.toLowerCase()) === 0);
+  }
+
+  pickDoctor() {
+    console.log(this.doctorCtrl.value)
+  }
 }
+
+const fakedResponseObj = [
+  {
+    doctor: "Dr. Ahmed Mohsen",
+    service: "Cardio",
+    patientRecord: "Alex",
+    date: "10/10/1995",
+    from: new Date(2018, 0, 10, 9),
+    to: new Date(2018, 0, 10, 12),
+    status: "ussual",
+    category: "regular medicine",
+    createdBy: "assistant",
+    createdAt: new Date(2017, 11, 10),
+    notes: ""
+  },
+  {
+    doctor: "Dr. Ahmed Mohsen",
+    service: "Cardio",
+    patientRecord: "Mira",
+    date: "3/5/2001",
+    from: new Date(2018, 0, 12, 12),
+    to: new Date(2018, 0, 12, 15),
+    status: "ussual",
+    category: "regular medicine",
+    createdBy: "assistant",
+    createdAt: new Date(2017, 11, 10),
+    notes: ""
+  }
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,88 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+    MatSnackBarModule,
+    MatChipsModule,
+    MatIconModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatAutocompleteModule,
+    MatSortModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatListModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonToggleModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatExpansionModule
+} from '@angular/material';
+import 'hammerjs';
+
+import 'flatpickr/dist/flatpickr.css';
+import * as flatpickr from 'flatpickr';
+import { FlatpickrModule, FLATPICKR } from 'angularx-flatpickr';
+import { NvD3Module } from 'ng2-nvd3';
+import 'd3';
+import 'nvd3';
 
 import { AppComponent } from './app.component';
+
+export function flatpickrFactory() {
+  return flatpickr;
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    FlatpickrModule.forRoot({
+      provide: FLATPICKR,
+      useFactory: flatpickrFactory
+    }),
+    NvD3Module,
+    MatSnackBarModule,
+    MatChipsModule,
+    MatIconModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatTableModule,
+    MatAutocompleteModule,
+    MatSortModule,
+    MatTooltipModule,
+    MatMenuModule,
+    MatListModule,
+    MatTabsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonToggleModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    MatExpansionModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
